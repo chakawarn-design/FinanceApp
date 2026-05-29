21257a2d637b948fe6c0093646f468b1b5c1b639
NoomFinance <chakawarn@gmail.com>
2026-05-29 14:15:19 +0700

    connect real api

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSGrYgov2wDOleB2ILwSZh7LiP3Xw_FWSBwuVt8N9eOFURGeJ--AbUVKf_6FxiV0qRlTfLGqxkYwM4Du8tjTMfkpqWhpbr6HuAm-K6z0y9o8OiloLupahXX5S1LhOmFhI-o6U2H3qEbF_9YD-dnZDCz44OEmJjk-4Ti_8KFUSO8J9E0Oz8rLV_02NIxk3KerIfxHpOTtIFxVq6NGz5EZcennqb95YijeSQ87zfzWKdHzb7wx3s&lib=MJr2XP3oQKglezWOE01x7urSTbnvLL5dA";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbyOUTYo8W19ci_OMXHJY7xmq0M8cD4BemZYzqiQ4SWT572dPB0YnDX7pLFhpSoG8nuG/exec";
 
 async function loadDashboard() {
 
   try {
 
     const response = await fetch(
-      API_URL + "&action=dashboard"
+      API_URL + "?action=dashboard"
     );
 
     const result = await response.json();
 
-    console.log(result);
+    console.log("ข้อมูลจาก API:", result);
 
   } catch (error) {
 
