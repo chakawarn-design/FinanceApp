67d04d24d5426a5284f5179474fc6ad497e8f0bc
NoomFinance <chakawarn@gmail.com>
2026-05-29 14:22:33 +0700

    connect real api

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbyOUTYo8W19ci_OMXHJY7xmq0M8cD4BemZYzqiQ4SWT572dPB0YnDX7pLFhpSoG8nuG/exec';
+
 async function loadDashboard() {
-  try {
-    const response = await fetch(API_URL + '?action=dashboard');
-    const data = await response.json();
+    try {
+        const response = await fetch(API_URL + '?action=dashboard');
+        const data = await response.json();
 
-    console.log(data);
+        console.log(data);
 
-  } catch (error) {
-    console.error('โหลดข้อมูลไม่สำเร็จ', error);
-  }
+    } catch (error) {
+        console.error('โหลดข้อมูลไม่สำเร็จ', error);
+    }
 }
+
+loadDashboard();
